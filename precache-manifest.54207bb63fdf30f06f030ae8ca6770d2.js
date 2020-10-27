b0481f8a27690a09f511b1e949a6a68ca34dbe49
RemediosPerez <rper0021@student.monash.edu>
2020-10-27 11:20:27 +1100

    Updates

---
+++ precache-manifest.54207bb63fdf30f06f030ae8ca6770d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6559e225e5a3a9905439291acb807ac",
+    "url": "/ex1/index.html"
+  },
+  {
+    "revision": "dca7bdc198ab1f503fe0",
+    "url": "/ex1/static/css/main.21bd0918.chunk.css"
+  },
+  {
+    "revision": "2e19b471e2af7bd1ca6b",
+    "url": "/ex1/static/js/2.f5df116d.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/ex1/static/js/2.f5df116d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dca7bdc198ab1f503fe0",
+    "url": "/ex1/static/js/main.073c0bac.chunk.js"
+  },
+  {
+    "revision": "71e554f9df3640ee29bc",
+    "url": "/ex1/static/js/runtime-main.dfebd1d6.js"
+  },
+  {
+    "revision": "e78d3d0558952175feb1d2061afc1875",
+    "url": "/ex1/static/media/vineyard.e78d3d05.jpg"
+  }
+]);
